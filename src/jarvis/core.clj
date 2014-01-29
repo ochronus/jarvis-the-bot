24964f358de44fe53c3dfb6ebc14615f733f377e
Csaba Okrona <ochronus@ochronus.com>
2014-01-29 16:45:19 +0100

    TeH CodeZ

---
+++ src/jarvis/core.clj
@@ -0,0 +1,24 @@
+(ns jarvis.core
+  (:require [xmpp :as xmpp]))
+
+(defn fetch-url [address]
+  (with-open [stream (.openStream (java.net.URL. address))]
+    (let  [buf (java.io.BufferedReader. 
+                (java.io.InputStreamReader. stream))]
+      (apply str (line-seq buf)))))
+
+(defn handle-message [message]
+  (let [body (:body message)
+        from-user (:from-name message)]
+    (if (clojure.string/blank? body) (fetch-url "http://api.sharedcount.com/?url=http://prezi.com/") (str "Hi " from-user ", you sent me '" body "'"))))
+
+
+
+(defn start-bot [] (xmpp/start-bot :username "12694_642587@chat.hipchat.com"
+                :password "Pazuzu666"
+                :host "chat.hipchat.com"
+                :domain "chat.hipchat.com"
+                :handler (var handle-message)
+                )
+)
+
---
+++ src/xmpp.clj
@@ -0,0 +1,19 @@
+(ns xmpp
+  (:require [xmpp.bot :as bot]))
+
+(def bots (atom {}))
+
+(defn stop-bot [& [name]]
+  (bot/stop (or name :default)))
+
+(defn start-bot [& opts]
+  (let [opts (merge {:name :default}
+                    (apply hash-map opts))
+        name (:name opts)]
+    (swap!
+     bots
+     (fn [bots]
+       (bot/stop (get bots name))
+       (assoc bots name (bot/start opts (get opts :handler (fn [m]))))))))
+
+(def restart-bot start-bot)
